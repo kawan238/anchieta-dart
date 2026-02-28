2c2a61becec6e1376e1aab3088ea0b834f201ebb
KAWAN PEREIRA DE ALMEIDA (2402175) <2402175@edu.anchieta.br>
2026-02-27 21:19:47 -0300

    Resolução dos exercicios

--- lib/exercicios.dart
+++ lib/exercicios.dart
@@ -1,49 +1,49 @@
 // 1. Soma
 num somar(num a, num b){
-  return 0;
+  return a + b;
 }
 
 // 2. Par
-bool isPar(int n) {
-  return false;
+bool isPar(int numero) {
+  return numero % 2 == 0;
 }
 
 // 3. Maior de idade
 bool podeEntrar(int idade){
-  return false;
+  return idade >= 18;
 }
 
 // 4. Celsius para Fahrenheit
 double converter(double c){
-  return 0.0;
+  return (c*9/5 ) + 32 ;
 }
 
 // 5. Saudação
 String saudar(String nome){
-  return "";
+  return ("Olá, $nome!");
 }
 
 // 6. Média
 double calcularMedia(List<double> notas) {
-  return 0.0;
+  return notas.isEmpty? 0.0 :notas.reduce((a, b) => a + b) / notas.length;
 }
 
 // 7. Filtrar Maiores
 List<int> buscarMaiores(List<int> numeros, int corte){
-  return [];
+ return numeros.where((n) => n > corte).toList();
 }
 
 // 8. Nulos (Null Safety)
 int tamanhoTexto(String? texto){
-  return 0;
+  return texto?.length ?? 0;
 }
 
 // 9. Carrinho com Desconto
 double fecharPedido(List<double> precos){
-  return 0.0;
+  return precos.isEmpty ? 0.0 : precos.reduce((a, b) => a + b) * (precos.reduce((a, b) => a + b) >500? 0.85 : 0.95);
 }
 
 // 10. Busca Case Insensitive
 bool contemNome(List<String> nomes, String busca){
-  return false;
+  return nomes.where((n) => n.toLowerCase() == busca.toLowerCase()).isNotEmpty;
 }
